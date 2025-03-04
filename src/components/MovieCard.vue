4d96a0f89b0173750e03be7c3e2fc7703f45195c
unknown <ramovic225@gmail.com>
2025-03-04 20:39:18 +0100

    props added

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,20 +1,27 @@
 <script setup>
 import { Icon } from "@iconify/vue";
+
+defineProps({
+    title: String,
+    year: Number,
+    rating: Number,
+    imageUrl: String
+})
+
 </script>
 <template>
     <div class="relative w-56 h-72 m-4 rounded-xl bg-gray-400 group overflow-hidden duration-300">
-        <img src="https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg" alt="movie-title"
-            class="w-full h-full object-cover">
+        <img :src="imageUrl" :alt="title" class="w-full h-full object-cover">
         <div
             class="absolute inset-0 translate-y-full group-hover:translate-y-0 bg-gray-800/90 flex flex-col justify-between duration-300 p-4">
             <div class="flex flex-col items-center justify-center gap-4 text-white text-2xl font-semibold">
                 <div class="flex flex-col items-center justify-center text-center">
-                    <h3 class="text-center">Kad porastem bicu kengur</h3>
-                    <p class="text-center">(2024)</p>
+                    <h3 class="text-center">{{ title }}</h3>
+                    <p class="text-center">{{ year }}</p>
                 </div>
                 <div class="flex gap-2 items-center justify-center">
                     <Icon icon="ic:baseline-star" width="24" height="24" color="yellow" />
-                    <span>8.2</span>
+                    <span>{{ rating }}</span>
                 </div>
             </div>
             <div class="text-white text-center text-lg">See more</div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,5 +4,6 @@ import MovieCard from '@/components/MovieCard.vue';
 </script>
 
 <template>
-  <MovieCard />
+  <MovieCard title="Prestige" imageUrl="https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg" rating="8.2"
+    year="2024" />
 </template>
