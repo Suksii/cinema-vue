0584caa50b455acdb3f60795c71f739a251818e4
unknown <ramovic225@gmail.com>
2025-03-15 23:14:26 +0100

    current year created with ref

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,8 +1,12 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const currentYear = ref(new Date().getFullYear());
+</script>
 <template>
   <div class="flex justify-center items-center gap-2 w-full h-22 bg-secondary">
-    <p class="text-white text-lg">Copyright 2025</p>
-    <p class="text-white text-lg">&copy;</p>
-    <p class="text-white text-lg">Šućo Ramović</p>
+    <p class="text-white text-lg">
+      Copyright {{ currentYear }} &copy; Šućo Ramović
+    </p>
   </div>
 </template>
