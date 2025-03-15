806b8e7e2fd743ea7373dc669eed7d5533bbb12d
unknown <ramovic225@gmail.com>
2025-03-15 22:51:07 +0100

    footer created

---
+++ src/components/FooterSection.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+<template>
+  <div class="flex justify-center items-center gap-2 w-full h-22 bg-secondary">
+    <p class="text-white text-lg">Copyright 2025</p>
+    <p class="text-white text-lg">&copy;</p>
+    <p class="text-white text-lg">Šućo Ramović</p>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { request } from "@/api";
-import MovieList from "@/components/MovieList.vue";
+import FooterSection from "@/components/FooterSection.vue";
 import Slider from "@/components/Slider.vue";
 import { Icon } from "@iconify/vue";
 import { ref, watchEffect } from "vue";
@@ -63,12 +63,6 @@ watchEffect(async () => {
     <div class="py-12">
       <Slider :moviesData="topRatedMoviesData" />
     </div>
-    <div class="flex items-center border-l-8 border-primary">
-      <h2 class="text-2xl px-4 font-bold text-white">Movies</h2>
-      <Icon icon="fe:arrow-right" width="24" height="24" class="text-primary" />
-    </div>
-    <div class="py-12">
-      <MovieList />
-    </div>
   </div>
+  <FooterSection />
 </template>
